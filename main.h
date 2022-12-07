5c99b9222f55dc36a841ee03fb0ac5d3a9ca9f46
spindouken <mlc9181209@gmail.com>
2022-12-07 10:40:08 -0600

    updated main.h with new functions

--- main.h
+++ main.h
@@ -8,9 +8,12 @@
 extern char environ;
 
 int main(argc, **argv, **envp);
-char *_read(void);
 char *_getenv(const char *name);
 int _PATHstrcmp(const char *s1, const char *s2);
+char *_read(void);
+char **tokenizer(char *buffer);
+int _strcount(char *str)
+char *_strdup(char *str)
 //helper functions
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
