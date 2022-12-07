c73f59ec415300593c0a1f2db27ee782b8bc2d53
spindouken <spindouken@gmail.com>
2022-12-06 20:21:46 -0600

    updated main.h libraries, functions, added environ

--- main.h
+++ main.h
@@ -2,8 +2,15 @@
 #ifndef MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+extern char environ;
 
 int main(argc, **argv, **envp);
+char *_read(void);
+char *_getenv(const char *name);
+int _PATHstrcmp(const char *s1, const char *s2);
 //helper functions
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
